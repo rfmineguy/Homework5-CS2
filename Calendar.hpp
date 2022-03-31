94075dd9198a0073663e5e38bcd427f2e27a8754
rfmineguy <rfmineguy@gmail.com>
2022-03-30 21:51:50 -0700

    Finished Calendar UML and DateTime UML

--- Calendar.hpp
+++ Calendar.hpp
@@ -1,6 +1,21 @@
+#include "Event.hpp"
+
 class Calendar {
     public:
+        Calendar(int sz = 100);
+        Calendar(const Calendar& cal);
+        ~Calendar();
+
+        Calendar& operator=(const Calendar&);
+        Event& operator[](int i);
+        Event& operator[](int i) const;
+        friend std::ostream& operator<<(std::ostream& os, const Calendar&);
+
+        void addEvent();
+        void removeEvent();
 
     private:
-
+        int* arraySize;
+        int* lastIndex;
+        Event** eventDynArray;
 };
