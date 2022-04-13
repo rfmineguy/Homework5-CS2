90222b399049012058e13fcaea415ce4c5ba2c9b
rfmineguy <rfmineguy@gmail.com>
2022-04-13 09:07:05 -0700

    Finished destructor for Calendar

--- Calendar.cpp
+++ Calendar.cpp
@@ -1,14 +1,24 @@
 #include "Calendar.hpp"
 #include <iostream>
 
-Calendar::Calendar(int sz) {
+Calendar::Calendar(int sz) 
+:arraySize(new int(sz)){
     
 }
 Calendar::Calendar(const Calendar& cal) {
 
 }
 Calendar::~Calendar() {
-
+    for (int i = 0; i < *arraySize; i++) {
+        delete eventDynArray[i];
+        eventDynArray[i] = nullptr;
+    }
+    delete eventDynArray;
+    eventDynArray = nullptr;
+    delete arraySize;
+    arraySize = nullptr;
+    delete lastIndex;
+    lastIndex = nullptr;
 }
 Calendar& Calendar::operator=(const Calendar& c) {
     *this->arraySize = *c.arraySize;
