75f69723f1eb8295c7f582353607a63de7d89941
Riley Fischer <rfmineguy@gmail.com>
2022-04-13 09:26:00 -0700

    Worked on Calendar.hpp/cpp. Started impl of destructor and operator<<

--- Calendar.cpp
+++ Calendar.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 
 Calendar::Calendar(int sz) 
-:arraySize(new int(sz)){
+:arraySize(new int(sz)), lastIndex(new int(-1)) {
     
 }
 Calendar::Calendar(const Calendar& cal) {
-
+    
 }
 Calendar::~Calendar() {
     for (int i = 0; i < *arraySize; i++) {
@@ -32,7 +32,16 @@ Event& Calendar::operator[](int i) const {
     return *eventDynArray[i];
 }
 std::ostream& operator<<(std::ostream& os, const Calendar& c) {
-    os << "Calendar << " << std::endl;
+    os << "Calendar contains " << *c.lastIndex << " events.\n";
+    os << "--------------------------------------------------\n";
+    for (int i = 0; i < *c.lastIndex; i++) {
+        os << "Event #" << i << "\n";
+        os << "Describption : \n";
+        os << "\t" << c.eventDynArray[i]->GetDescription() << "\n";
+        os << "Date and Time info : \n";
+        os << "\t" << c.eventDynArray[i]->GetDateTime() << "\n";
+    }
+    os << "--------------------------------------------------\n";
     return os;
 }
 void Calendar::addEvent() {
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-g++ -std=c++11 main.cpp Calendar.cpp DateTime.cpp
+g++ -std=c++11 main.cpp Calendar.cpp DateTime.cpp Event.cpp
 ./a.out
